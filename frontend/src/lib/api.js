eecdd5b25fe16120c3e51b58bbe68dbf204513df
Braeden <bduval3@my.bcit.ca>
2026-04-01 21:44:13 -0700

    fix api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 export const API_BASE_URL = (process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000').replace(/\/+$/, '');
+const isNgrokBaseUrl = /\.ngrok(-free)?\.(app|dev|io)$/i.test(API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL
 });
+
+if (isNgrokBaseUrl) {
+  api.defaults.headers.common['ngrok-skip-browser-warning'] = '1';
+}
 
 export default api;
